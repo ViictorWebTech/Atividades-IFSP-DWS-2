c9ba2ed27b887b644726434280fee7ff977670d4
ViictorWebTech <vwebtech777@gmail.com>
2023-06-03 16:36:09 -0300

    :ok_hand: style: Renomeação de Diretórios

---
+++ Aula 16 - Funções Built-in e Coleções (Arrays)/funcao.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Praticando 1 - Funções Built-in e Coleções (Arrays)</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <style>
+        header {
+            margin-top: .5em;
+        }
+        button{
+            margin-right: .5em;
+        }
+    </style>
+
+</head>
+
+<body>
+<div class="container">
+    <header>
+        <div class="header">
+            <h1>Praticando 1 - Funções Built-in e Coleções (Arrays)</h1>
+            <hr>
+        </div>
+    </header>
+
+    <main>
+        <div class="conteudo-principal">
+        <form method="POST">
+        <div class="row">
+            <div class="col-6">
+                <div class="mb-3">
+                    <label for="mensagem" class="form-label">Texto: </label>
+                    <textarea rows="3" class="form-control" name="mensagem" title="mensagem" required> </textarea>
+                </div>
+            </div>
+
+        </div>
+        <div class="d-flex">
+            <button type="submit" class="btn btn-success">Enviar</button>
+            <button type="reset" class="btn btn-warning">Limpar</button>
+        </div>
+    </form>
+        </div>
+    </main>
+
+
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
